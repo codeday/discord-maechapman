2724da08974c99fd1eeeed9a963dae84eac373fa
PlaidMoose <njlandry05@gmail.com>
2021-03-09 16:17:28 -0600

    Linked the cog command.

--- src/cogs/send.py
+++ src/cogs/send.py
@@ -1,12 +1,23 @@
+from datetime import date
+
 import gspread
 import discord
-from discord.ext import commands
+from discord import client, message
+from discord.ext import commands, tasks
+
 
 
 class SendCog(commands.Cog, name="Send"):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name='m')
+    async def check(self, ctx):
+        gc = gspread.service_account(filename='service_account.json')
+        sheet = gc.open('Discord Announcements')
+        worksheet = sheet.get_worksheet(0)
+        list_of_dicts = worksheet.get_all_records()
+        await ctx.send(list_of_dicts)
 
 def setup(bot):
     bot.add_cog(SendCog(bot))
--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
+import traceback
+import logging
 from os import getenv
 
 import discord
 import discord.ext.commands as commands
+import gspread
+from discord import message
+
 
 intents = discord.Intents.default()
 intents.members = True
@@ -13,5 +18,24 @@ bot = commands.Bot(command_prefix='m', intents=intents)
 async def on_ready():
     print('Ready')
 
+initial_cogs = [
+    "cogs.send"
+]
+loaded_cogs = []
+
+# Here we load our extensions(cogs) listed above in [initial_extensions].
+failed_cogs = []
+for cog in initial_cogs:
+    # noinspection PyBroadException
+    try:
+        bot.load_extension(cog)
+        logging.info(f"Successfully loaded extension {cog}")
+        loaded_cogs.append(cog)
+    except Exception as e:
+        failed_cogs.append(f'''Failed to load extension {cog}.
+Traceback: {traceback.format_exc()}''')
+        logging.exception(
+            f"Failed to load extension {cog}.", exc_info=traceback.format_exc()
+        )
 
 bot.run(BOT_TOKEN, bot=True, reconnect=True)
