351e916d7fed6ae57d1058f80a2a8164ffc22465
PlaidMoose <njlandry05@gmail.com>
2021-03-11 15:00:42 -0600

    records now updates every 30 minutes

--- src/cogs/send.py
+++ src/cogs/send.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 from dotenv import load_dotenv
 import gspread
 import discord
@@ -9,44 +9,41 @@ from discord.ext import commands, tasks
 class SendCog(commands.Cog, name="Send"):
     def __init__(self, bot):
         self.bot = bot
-        self.events = []
+        self.records = []
+        self.check.start()
         self.update.start()
+        self.gc = gspread.service_account(filename='service_account.json')
+        self.sheet = self.gc.open('Discord Announcements')
+        self.worksheet = self.sheet.get_worksheet(0)
 
     def cog_unload(self):
         self.update.cancel()
+        self.check.cancel()
 
-    @commands.command(name='m')
-    async def check(self, ctx):
-        load_dotenv()
-        gc = gspread.service_account(filename='service_account.json')
-        sheet = gc.open('Discord Announcements')
-        worksheet = sheet.get_worksheet(0)
-        list_of_dicts = worksheet.get_all_records()
-        await ctx.send(list_of_dicts)
+    @tasks.loop(minutes=30)
+    async def check(self):
+        print('Updating')
+        self.records = self.worksheet.get_all_records()
 
     @tasks.loop(seconds=60)
     async def update(self):
         print("Checking")
-        gc = gspread.service_account(filename='service_account.json')
-        sheet = gc.open('Discord Announcements')
-        worksheet = sheet.get_worksheet(0)
-        records = worksheet.get_all_records()
-        for index, record in enumerate(records):
-            (timestamp) = str(datetime.datetime.today())
-
-            if record['Posted'] == 'FALSE':
-                if str(record['Time']) in timestamp:
-                    message_channel = await self.bot.fetch_channel(int(record['ChannelID']))
-                    await message_channel.send(str(record['Announcement']))
-                    announcement_cell = worksheet.find(str(record['Announcement']))
+        for index, self.record in enumerate(self.records):
+            timestamp = datetime.today().strftime("%Y-%m-%d %H:%M")
+            print(timestamp)
+            if self.record['Posted'] == 'FALSE':
+                if str(self.record['Time']) in timestamp:
+                    message_channel = await self.bot.fetch_channel(int(self.record['ChannelID']))
+                    await message_channel.send(str(self.record['Announcement']))
+                    announcement_cell = self.worksheet.find(str(self.record['Announcement']))
                     announcement_row = announcement_cell.row
-                    worksheet.update_cell(announcement_row, 5, "TRUE")
+                    self.worksheet.update_cell(announcement_row, 5, "TRUE")
 
-                    worksheet.delete_row(announcement_row)
-                    records[index]['Posted'] = 'TRUE'
-                    finished_worksheet = sheet.get_worksheet(1)
+                    self.worksheet.delete_row(announcement_row)
+                    self.records[index]['Posted'] = 'TRUE'
+                    finished_worksheet = self.sheet.get_worksheet(1)
                     finished_worksheet.append_row(
-                        [str(record[val]) for val in ("ChannelName", "ChannelID", "Announcement", "Time", "Posted")])
+                        [str(self.record[val]) for val in ("ChannelName", "ChannelID", "Announcement", "Time", "Posted")])
 
                 continue
 
